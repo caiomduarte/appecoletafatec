96a238bd1fb142ebca1abcb353551576a59bf080
Caio Duarte <md.caio@yahoo.com.br>
2022-04-26 18:18:51 -0300

    Projeto Ecoleta Fatec 2022

---
+++ src/pages/Detail/index.jsx
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Detail(){
+    return(
+        <View>
+            <Text>Tela Detail</Text>            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+});
+
+
---
+++ src/pages/Points/index.jsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Points(){
+    return(
+        <View style={styles.container}>
+            <Text>Tela Points</Text>            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems:'center'
+    }
+});
+
